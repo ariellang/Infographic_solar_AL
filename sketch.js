76f0a89e114d9e7cd059903b0b3eca19290cef7b
ariellang <143737182+ariellang@users.noreply.github.com>
2024-06-04 09:05:31 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -41,8 +41,8 @@ let fontT;
 
 function preload(){
   imgBG =loadImage('Solar system 2.png');
-  fontN =loadFont("Nunito/Nunito-VariableFont_wght.ttf");
-  fontT =loadFont("Tinos/Tinos-Regular.ttf")
+  fontN =loadFont("Nunito-VariableFont_wght.ttf");
+  fontT =loadFont("Tinos-Regular.ttf")
   milky = loadImage("Galaxy.jpg");
   LoI = loadImage("Locate icon.png");
   solarI =loadImage("Solar system icon.png");
